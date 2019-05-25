a0f3ae9bc22d36404c3b11c6610d70bc4fb35238
NataSh <N.Shepina@bk.ru>
2019-05-25 21:01:40 +0300

    Change CompileResponseDto

--- PapiServiсe/Controllers/PApiController.cs
+++ PapiServiсe/Controllers/PApiController.cs
@@ -25,29 +25,43 @@ namespace PapiServiсe.Controllers
             return "value";
         }
 
-       
+
         [HttpPost]
-        public Face Post(IFormFile jpgRequest)
-        {
-            
+        public CompileResponseDto Post(IFormFile jpgRequest)
+        { 
             var stream = jpgRequest.OpenReadStream();
             var name = jpgRequest.FileName;
-            
+
             var client = new FakeCognitiveApiClient();
             var task = client.GetFaceListAsync(null);
             task.Wait();
-            var cognitiveAnswer = task.Result.FirstOrDefault();
+            var cognitiveAnswer = task.Result;
+            FaceDto output = cognitiveAnswer
+                .Select(o => new FaceDto
+                {
+                    Rectangle = new FaceRectangle
+                    {
+                        Top = o.FaceRectangle.Top, Height = o.FaceRectangle.Top, Left = o.FaceRectangle.Left,
+                        Width = o.FaceRectangle.Width
+                    },
+                    Age = o.FaceAttributes.Age,
+                    Gender = o.FaceAttributes.Gender,
+                    Smile = o.FaceAttributes.Smile
+                }).FirstOrDefault();
+
+
+
+            // var cognitiveAnswer = task.Result.FirstOrDefault();
             if (cognitiveAnswer == null)
             {
                 return null;
             }
 
-            var face = new Face();
-            return face;
-         
-            
-            var response = HttpContext.Response.Headers;
-        //    return StatusCode(200);
+            var face = new FaceDto { };
+            return new CompileResponseDto {Faces = outputs};
+
+
+            //var response = HttpContext.Response.Headers;
         }
 
         // PUT api/values/5
